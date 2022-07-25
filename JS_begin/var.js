93ef0b25019238afe7f87a6029ec5f3a3aab5b46
chyorik <109052013+chyorik@users.noreply.github.com>
2022-07-25 10:53:00 +0900

    Add files via upload

---
+++ JS_begin/var.js
@@ -0,0 +1,20 @@
+let x=1;
+console.log(x);
+function f(){
+    let y;
+    x = 10;
+    y = 20;
+    z = 30;
+    console.log(x);
+    console.log(y);
+    console.log(z);
+
+    if(y == 20){
+        let b = 40;
+        b++;
+    console.log(b);
+    }
+}
+f();
+console.log(y);
+console.log(x);
